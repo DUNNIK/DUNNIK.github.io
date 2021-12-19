304c1c54dfd6c12f62397a0de7f06efad46b6b5d
DUNNIK <dns.56@mail.ru>
2021-12-19 15:23:23 +0300

    add: lab5 and lab6

--- js/mockGallery.js
+++ js/mockGallery.js
@@ -10,7 +10,10 @@ function addPage(currentPage) {
     document.querySelector('.main__loader').setAttribute('style', 'display: inline;');
 
     fetch('https://jsonplaceholder.typicode.com/photos?_limit=8&_page=' + currentPage)
-        .then(response => response.json())
+        .then(response => response.json()
+        ).catch((error) => {
+        console.log(error, "er");
+        document.querySelector('.fa').setAttribute('style', 'display: inline;')})
         .then(data => {
                 for (let item of data) {
                     addPhoto(item['title'], item['thumbnailUrl'], item['url'])
@@ -18,7 +21,8 @@ function addPage(currentPage) {
                 document.querySelector('.main__loader').setAttribute('style', 'display: none;');
                 document.querySelector('.fa').setAttribute('style', 'display: none;');
             }
-        ).catch((error) => {
+        )
+        .catch((error) => {
         console.log(error);
         document.querySelector('.fa').setAttribute('style', 'display: inline;')
     });
